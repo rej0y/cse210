8df35be661c1168570519f8e7113d8c86330d551
rej0y <zhoushengjian1@gmail.com>
2025-10-11 23:02:50 -0600

    Update Journal Extra Comment.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Security.Cryptography.X509Certificates;
 
+
+
+// This program can also choose 20 random journal prompts and store the data into a JSON file.
 class Program
 {
     static void Main(string[] args)
