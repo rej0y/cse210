e2e00203563275b4bd67547355e15970a028e0e2
rej0y <zhoushengjian1@gmail.com>
2025-10-14 23:39:30 -0600

    Upload Learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Fraction fraction1 = new Fraction();
+        // Fraction fraction2 = new Fraction(6);
+        // Fraction fraction3 = new Fraction(6, 7);
+
+        // Console.WriteLine($"{fraction1.GetTop()}\n{fraction2.GetTop()}\n{fraction3.GetTop()}");
+        // Console.WriteLine($"{fraction1.GetBottom()}\n{fraction2.GetBottom()}\n{fraction3.GetBottom()}");
+        // Console.WriteLine($"{fraction1.GetFractionString()}\n{fraction2.GetFractionString()}\n{fraction3.GetFractionString()}");
+        // Console.WriteLine($"{fraction1.GetDecimalValue()}\n{fraction2.GetDecimalValue()}\n{fraction3.GetDecimalValue()}");
+
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(5);
+        Fraction fraction3 = new Fraction(3, 4);
+        Fraction fraction4 = new Fraction(1, 3);
+
+        Console.WriteLine($"{fraction1.GetFractionString()}\n{fraction1.GetDecimalValue()}");
+        Console.WriteLine($"{fraction2.GetFractionString()}\n{fraction2.GetDecimalValue()}");
+        Console.WriteLine($"{fraction3.GetFractionString()}\n{fraction3.GetDecimalValue()}");
+        Console.WriteLine($"{fraction4.GetFractionString()}\n{fraction4.GetDecimalValue()}");
+
     }
 }
