70f702ddb0555724e745e1c003726cb19c3a4063
rej0y <zhoushengjian1@gmail.com>
2026-01-08 14:52:08 -0700

    test update

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = ".NET 10 Development Shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.dotnet-sdk_10
+        ];
+      };
+    };
+}
