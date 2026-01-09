af05fe7471444fbfc5eee0b182e3be9e8cb78c1d
rej0y <zhoushengjian1@gmail.com>
2026-01-09 15:37:02 -0700

    Upload prep 1.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask for the first name.
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Ask for the last name.
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Output the result.
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
